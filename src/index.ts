de892d9346d085edfc164d3bd5ceffaf3f83a5e3
Arthur E. Jones <PartyLich@gmail.com>
2022-07-26 13:22:32 -0500

    feat: implement main entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export {};
+import MemoryStore from 'storage/mem';
+import RedirectService from 'service';
+import ExpressServer from 'rest/express/app';
+import { Config } from 'types';
+
+const DEFAULT_CONFIG: Config = {
+  port: 3000,
+  hostname: 'localhost',
+};
+
+const loadConfig = (): Config => DEFAULT_CONFIG;
+
+const run = async () => {
+  const config = loadConfig();
+  const repo = MemoryStore();
+  const service = RedirectService(repo);
+  const server = ExpressServer(service);
+
+  console.log(`Listening on ${ config.hostname } : ${ config.port } ...`);
+  server.listen(config.port, config.hostname);
+};
+
+run().catch((err) => {
+  console.log('Error starting app:', err);
+  process.exit(1);
+});
